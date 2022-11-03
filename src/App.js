e1cfa381abd3ecfe55c15834bb0a950b89fc797d
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2022-11-04 02:56:21 +0300

    edited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+// import Header from "./companents/Header"; 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (  // kapasayıcı divi kullanmak istemiyorsan div yerine bu "<>.. </>" fragment ifadesini ya da " <React.Fragment>...</React.Fragment>" ifadesini kullanabilirsin bu iki kullanım da aynı amaca hizmet eder
+    <div className="test">
+      <div>
+        <p>Test</p>
+      </div>
+
+      <label htmlFor="test">Test</label>
+
+      <div>Selam</div>
     </div>
   );
 }
-
 export default App;
+//jsx ifadesi için de class tanımlaması ' className="blabla" ' şeklinde yapılır.
+//label için de for ifadesi kullanırız. bunun jsx de ki karşılığı htmlFor="blabla" dır.
