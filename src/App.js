6c03dfae7f8401a2eef575df7bc35ccaea5a96ba
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2022-11-05 15:16:49 +0300

    bir objeyi property olarak gönderme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import User from "./companents/User";
 
-//PROPS YAPISI
-//companente anahtar değer çiftleriyle veriyi gönderip orada fonksiyonun parametresi olarak verileri yakalıyoruz.
-//veriler dizi olarak gidiyor yani aldığımız veriyi örneğin props.name, props.surname, props.age şeklinde yakalıyoruz.
+//BİR OBJEYİ, PROERTY OLARAK GONDERME
+const user = {
+  name: "ozgur",
+  city: "Izmir",
+  age: 22,
+}
+
 function App() {
   return (
     <div>
-      <User name="Ozgur VURGUN" city="Izmir" age={22} />
-      <User name="Ece Sensoz" city="Izmir" age={22} />
-      <User name="Irem Keskin" city="Istanbul" age={25} />
+
+      {/*veriyi dizi olarak gönderme */}
+      <User data={user} />
     </div>
   );
 }
--- src/companents/User.js
+++ src/companents/User.js
@@ -2,16 +2,43 @@ import React from 'react';
 
 //PROPS YAPISI
 
-function User(props) {
-    console.log(props);
+// function User({ data:{name, age, city} }) {
+//     return (
+//         <div>
+//             User
+//             <div>İsim: {name}</div>
+//             <div>Yaş: {age}</div>
+//             <div>Şehir: {city}</div>
+//             <hr />
+//         </div>
+//     );
+// }
+
+
+//en çok bu kullanımı sevdim. kısaca app.js ten data={user} geliyor. bunu {data} bu şekilde parametre alıp data.name, data.age şeklinde değerlerle erişiyoruz
+//diğer kullanımlarda aynı işi yapıyor. sevdiğini seç
+function User({data}) {
     return (
         <div>
             User
-            <div>İsim: {props.name}</div>
-            <div>Yaş: {props.age}</div>
-            <div>Şehir: {props.city}</div>
+            <div>İsim: {data.name}</div>
+            <div>Yaş: {data.age}</div>
+            <div>Şehir: {data.city}</div>
             <hr />
         </div>
     );
 }
+
+// function User(props) {
+//     const data = props.data;
+//     return (
+//         <div>
+//             User
+//             <div>İsim: {data.name}</div>
+//             <div>Yaş: {data.age}</div>
+//             <div>Şehir: {data.city}</div>
+//             <hr />
+//         </div>
+//     );
+// }
 export default User;
